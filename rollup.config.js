3cc538d5f4b69d8a49105702d7d2dfb6dea4de75
sungwon.cha <mygenie3@naver.com>
2019-08-21 00:40:49 +0900

    fix

---
+++ rollup.config.js
@@ -0,0 +1,80 @@
+import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import postcssFlexbugsFixes from 'postcss-flexbugs-fixes';
+import postcssPresetEnv from 'postcss-preset-env';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+const pkg = require('./package.json');
+
+const entry = 'src/core';
+
+// const external = Object.keys(pkg.dependencies);
+const external = id => !id.startsWith('.') && !id.startsWith('/');
+
+const rollupConfig = {
+  input: `${entry}/index.ts`,
+  plugins: [
+    typescript({
+      // https://github.com/ezolenko/rollup-plugin-typescript2
+      clean: true,
+      objectHashIgnoreUnknownHack: true,
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true,
+          declarationDir: 'dist',
+          allowJs: false,
+          isolatedModules: false,
+        },
+        include: [entry],
+      },
+    }),
+    babel({
+      runtimeHelpers: true,
+      presets: [ '@babel/preset-react'],
+      // plugins: ['@babel/transform-runtime'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    postcss({
+      plugins: [
+        postcssFlexbugsFixes,
+        postcssPresetEnv({
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        }),
+      ],
+    }),
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    commonjs(),
+  ],
+  external,
+};
+
+const cjsConfig = {
+  ...rollupConfig,
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+    sourcemap: true,
+  },
+};
+
+const esConfig = {
+  ...rollupConfig,
+  output: {
+    file: pkg.module,
+    format: 'es',
+    sourcemap: true,
+  },
+};
+
+export default [
+  cjsConfig,
+  esConfig,
+];
